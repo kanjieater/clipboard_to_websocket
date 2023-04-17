09847624ca88e6109fa8d6a7bff2611877a4c98a
KanjiEater <kanjieat3r@gmail.com>
2023-04-17 12:52:29 -0500

    cleaned up readme and added params

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
-# clip_to_ws1
+# Clipboard_to_WebSocket
 
-Run with:
-`python __init__.py`
+A simple Python script that exposes your clipboard data to a WebSocket server.
 
-Expects to be connected to on:
-`ws://localhost:6678`
+## Usage
+
+- Clone the repository
+- Install dependencies with `pip install -r requirements.txt`
+- Start the WebSocket server with `python __init__.py`
+- Connect to the server using the WebSocket protocol. The clipboard data will be sent to the client as soon as it changes.
+
+## Configuration
+You can configure the WebSocket server host, port, and clipboard poll interval by passing command line arguments when starting the script:
+
+```bash
+Copy code
+usage: clip_to_ws.py [-h] [--host HOST] [--port PORT] [--poll-interval POLL_INTERVAL]
+```
+
+Expose clipboard data to a WebSocket server.
+
+```bash
+optional arguments:
+  -h, --help            show this help message and exit
+  --host HOST           WebSocket server host (default: localhost)
+  --port PORT           WebSocket server port (default: 6678)
+  --poll-interval POLL_INTERVAL
+                        Poll interval for clipboard data (default: 0.1)
+```
+
+Example:
+
+```python
+python __init__.py --host localhost --port 6678 --poll-interval 0.1
+```
+## License
+
+This software is released under the MIT License. See LICENSE for more information.
--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
+import argparse
 import asyncio
 import websockets
 import pyperclip
 
-async def send_clipboard_data(websocket, path):
+
+async def send_clipboard_data(websocket, path, poll_interval):
     last_clipboard_contents = ""
     last_send_status = True
+    print(f"Connected...")
     while True:
         clipboard_contents = pyperclip.paste()
         if clipboard_contents != last_clipboard_contents and last_send_status:
@@ -14,13 +17,29 @@ async def send_clipboard_data(websocket, path):
                 last_send_status = True
             except Exception as e:
                 last_send_status = False
-                with open("error.log", "a") as f:
-                    f.write(str(e) + "\n")
+                with open("error.log", "r") as f:
+                    last_error = f.readline().strip()
+                if str(e) != last_error:
+                    with open("error.log", "w") as f:
+                        f.write(str(e) + "\n")
         elif clipboard_contents != last_clipboard_contents and not last_send_status:
             last_send_status = True
-        await asyncio.sleep(0.3)
+        await asyncio.sleep(poll_interval)
 
-start_server = websockets.serve(send_clipboard_data, "localhost", 6678)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+async def ws_handler(websocket, path):
+    await send_clipboard_data(websocket, path, args.poll_interval)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Expose clipboard data to a WebSocket server.')
+    parser.add_argument('--host', type=str, default='localhost', help='WebSocket server host (default: localhost)')
+    parser.add_argument('--port', type=int, default=6678, help='WebSocket server port (default: 6678)')
+    parser.add_argument('--poll-interval', type=float, default=0.1, help='Poll interval for clipboard data (default: 0.1)')
+    args = parser.parse_args()
+
+    websocket_path = f"{args.host}:{args.port}"
+    start_server = websockets.serve(ws_handler, args.host, args.port)
+    print(f"Starting {'ws://' + websocket_path}")
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
